1cd9581c91c58d0d63b2a952342ef54325bba2f4
Tejas Aghade <tejas@thefitway.io>
2024-10-13 15:59:04 +0530

    emergency commit

--- src/app/pages/Skills.tsx
+++ src/app/pages/Skills.tsx
@@ -1,11 +1,29 @@
 import './css/skills.css';
 
 import skills from '../data/skills.data'
+import { motion, useAnimation, useInView } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 export default function Skills() {
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once : true});
+
+    const mainControl = useAnimation();
+
+    useEffect(()=>{
+        console.log(isInView);
+        
+        if(isInView){
+            mainControl.start("visible");
+        }
+    }, [isInView]); 
+  
+
     return (
         <>
-            <section className="skills-section" id='skills'>
+        
+            <section ref={ref} className="skills-section" id='skills'>
                 <div className="heading">
                     <h2>What i know</h2>
                 </div>
@@ -13,7 +31,7 @@ export default function Skills() {
                         {
                             skills.map((sCat, index) => {
                                 return (
-
+                                    
                                     <div key={sCat.skillCategory + " " + index} className="cat-card m-2">
 
                                         <div className="skil-heading">
@@ -25,12 +43,22 @@ export default function Skills() {
                                                 sCat.skills.map(skill => {
                                                     return (
                                                         <div key={skill.name} className="skill flex flex-col justify-center items-center">
-                                                            <div className="skill-img flex justify-center items-center">
-                                                                <img src={skill.icon} alt="gf" />
-                                                            </div>
-                                                            <div className="skill-title">
-                                                                <span>{skill.name}</span>
-                                                            </div>
+                                                            <motion.div
+                                                                variants={{
+                                                                hidden: {opacity: 0, y : 40},
+                                                                visible : {opacity : 1, y : 0}
+                                                                }}
+                                                                initial = "hidden"
+                                                                animate={mainControl}
+                                                                transition={{duration : 0.5, delay : 0.25}}
+                                                            > 
+                                                                <div className="skill-img flex justify-center items-center">
+                                                                    <img src={skill.icon} alt="gf" />
+                                                                </div>
+                                                                <div className="skill-title">
+                                                                    <span>{skill.name}</span>
+                                                                </div>
+                                                            </motion.div>
                                                         </div>
                                                     )
                                                 })
@@ -38,12 +66,14 @@ export default function Skills() {
                                         </div>
 
                                     </div>
+                                    
                                 )
                             })
 
                         }
                 </div>
             </section>
+            
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Navbar from './app/components/Navbar.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Navbar/>
     <App />
   </React.StrictMode>,
 )
